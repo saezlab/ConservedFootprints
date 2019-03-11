02a0320cc49b2dc519bd712eb47f9848f9c1b361
Christian <cholland2408@gmail.com>
2019-03-11 11:08:14 +0100

    short scrip to build complete full progeny matrix

---
+++ data/progeny_benchmark/models/build_full_matrix.R
@@ -0,0 +1,14 @@
+# load original full matrix of PROGENy (11 pathways)
+o = get(load("data/progeny_benchmark/models/sub/model_matrix.RData"))$assocs %>%
+  as_tibble()
+
+# load extended full matrix with the 3 new pathways and filter for them
+e = read_csv("data/progeny_benchmark/models/sub/full_model_extended.csv") %>%
+  select(-X1) %>%
+  filter(pathway %in% c("Androgen", "Estrogen", "WNT"))
+
+# combine both matrices to a the full matrix containing all 14 pathways
+full_matrix = bind_rows(o, new_pws)
+
+write_csv(full_matrix, 
+          "data/progeny_benchmark/models/progeny_matrix_human_full_v1.csv")
